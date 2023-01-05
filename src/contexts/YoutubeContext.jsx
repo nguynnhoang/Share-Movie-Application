d7a286694275b276ef9e08fe1a196410f1ee8c68
Nguyen Hoang <dopnguyen12@gmail.com>
2023-01-05 21:34:53 +0700

    update Share movie page

--- src/contexts/YoutubeContext.jsx
+++ src/contexts/YoutubeContext.jsx
@@ -1,4 +1,5 @@
 import { createContext } from "react";
+import { config } from "../config";
 
 export const YoutubeContext = createContext({
     shareVideo: (url) => { }
@@ -9,12 +10,34 @@ const youtubeUrlPattern =  /^((http|https)\:\/\/)?(www\.youtube\.com|youtu\.?be)
 const YoutubeProvider = ({ children }) => {
 
     const getVideoIdFromYoutubeUrl = (url) => {
-        console.log(url.match(youtubeUrlPattern))
+        // Promise is a constructor
+        return new Promise((resolve, reject) => {
+            const result = url.match(youtubeUrlPattern)
+            if (result === null) reject(new Error('Youtube Url is not valid'))
+            resolve(result[6])
+        })
     }
 
-    const shareVideo = (url) => {
-        console.log(url)
-        getVideoIdFromYoutubeUrl(url)
+    const validateVideoId = (videoId) => {
+        
+        return new Promise((resolve, reject) => {
+            fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${config.youtubeApiKey}`)
+                .then((response) => response.json())
+                .then(data => {
+                    //console.log(data) 
+                    // data trả về đầy đủ thông tin của video
+                    if (data.items.length === 0) reject(new Error('Youtube video not found'))
+                    resolve(data)
+                })
+        })
+    }
+
+    const shareVideo = async (url) => {
+        //console.log(url)
+        const videoId = await getVideoIdFromYoutubeUrl(url)
+        const youtubeData = await validateVideoId(videoId)
+        //console.log(videoId)
+        console.log(youtubeData)
     }
 
     const value = {shareVideo}
--- src/pages/ShareMoviePage/index.jsx
+++ src/pages/ShareMoviePage/index.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Button, Card, CardBody, CardHeader, Col, Form, FormGroup, Input, Label, Row } from "reactstrap"
+import { Alert, Button, Card, CardBody, CardHeader, Col, Form, FormGroup, Input, Label, Row } from "reactstrap"
 import { YoutubeContext } from "../../contexts/YoutubeContext";
 
 
@@ -9,6 +9,8 @@ import { YoutubeContext } from "../../contexts/YoutubeContext";
 const ShareMoviePage = () => {
 
     const { shareVideo } = useContext(YoutubeContext)
+
+    const [err, setError] = useState('')
 
     const [formValue, setFormValue] = useState({
         youtubeURL: ''
@@ -20,9 +22,15 @@ const ShareMoviePage = () => {
         console.log(formValue)
     }
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
-        shareVideo(formValue.youtubeURL)
+        if (err) setError('')
+        try {
+            await shareVideo(formValue.youtubeURL)
+        } catch (error) {
+            setError(error.message)
+            //console.log(error.message)
+        }
     }
 
     return (
@@ -32,22 +40,20 @@ const ShareMoviePage = () => {
                     <CardHeader>Share a youtube video</CardHeader>
                     <CardBody>
                         <Form onSubmit={onSubmit}>
-                        <FormGroup row>
-                            <Label for="youtubeURL" sm={3}>Youtube URL</Label>
-                            <Col sm={8}>
-                                <Input
-                                    id="youtubeURL"
-                                    name="youtubeURL"
-                                    placeholder="Youtube url ..."
-                                    type="url"
-                                    value={formValue.youtubeURL}
-                                    onChange={onChange}
-                                />
-
-                                <Button color="success" block className="mt-2">
-                                    Share
-                                </Button>
-                            </Col>
+                            {err && <Alert color="danger">{err}</Alert>}
+                            <FormGroup row>
+                                <Label for="youtubeURL" sm={3}>Youtube URL</Label>
+                                <Col sm={8}>
+                                    <Input
+                                        id="youtubeURL"
+                                        name="youtubeURL"
+                                        placeholder="Youtube url ..."
+                                        type="url"
+                                        value={formValue.youtubeURL}
+                                        onChange={onChange}
+                                    />
+                                    <Button color="success" block className="mt-2">Share</Button>
+                                </Col>
                             </FormGroup>
                         </Form>
                     </CardBody>
